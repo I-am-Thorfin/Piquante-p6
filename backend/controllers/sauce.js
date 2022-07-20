b71ad9c4ee1877143b29ac51e43556ce9cacbc25
I-am-Thorfin <89223429+I-am-Thorfin@users.noreply.github.com>
2022-07-20 09:08:07 +0200

    Progression - problème pour voir une sauce unique

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -46,6 +46,23 @@ exports.modifySauce = (req, res, next) => {
         .catch(error => res.status(400).json({ error }))
 };
 
+/* Suppression d'une sauce */
+
+exports.deleteSauce = (req, res, next) => {
+    Sauce.findOne({ _id: req.params.id })
+        .then(sauce => {
+            const filename = sauce.imageUrl.split('/images/')[1]
+            fs.unlink(`images/${filename}`, () => {
+                Sauce.deleteOne({ _id: req.params.id })
+                    .then(() => res.status(200).json({ message: 'Sauce supprimée !' }))
+                    .catch(error => res.status(400).json({ error: error }))
+            })
+        })
+        .catch(error => res.status(500).json({ error }))
+}
+
+
+
 
 
 
